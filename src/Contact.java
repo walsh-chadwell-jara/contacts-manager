d3483b7fa8ba4ac6adce7d88a7fb037178d934bc
Karla Jara <jara.karla.43@gmail.com>
2021-05-11 11:28:47 -0500

    added methods for contact information

--- src/Contact.java
+++ src/Contact.java
@@ -9,11 +9,24 @@ public class Contact {
         return (firstName + " " + lastName + " " + telephoneNumber);
     }
 
-    public void setFullName(String firstName, String lastName){
+    public void setContactInfo(String firstName, String lastName, String telephoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Contact name entered " + firstName + " " + lastName);
+        this.telephoneNumber = telephoneNumber;
+        System.out.println("Contact name entered " + this.firstName + " " + this.lastName + "\n telephone: " + this.telephoneNumber);
     }
 
+//    public String removeContact(){
+//        this.firstName
+//    }
+
+    public static void main(String[] args){
+        Contact contact_1 = new Contact();
+        contact_1.setContactInfo("Andrew", "Walsh", "210-456-9090");
+        Contact contact_2 = new Contact();
+        contact_2.setContactInfo("Karla", "Jara", "210-369-3333");
+
+
+    }
 }
 
