8bdb055c122ab5b0f06f6990e761a300f03c0c28
Karla Jara <jara.karla.43@gmail.com>
2021-05-11 14:24:17 -0500

    refactored Contact file

--- src/contacts/Contact.java
+++ src/contacts/Contact.java
@@ -5,25 +5,35 @@ public class Contact {
     private String lastName;
     private String telephoneNumber;
 
-    public String getCompleteInfo(){
-        return ("Contact name: " + firstName + " " + lastName + "\nTelephone number: " + telephoneNumber + "\n");
+    
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setContactInfo(String firstName, String lastName, String telephoneNumber){
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
-        this.lastName = lastName;
-        this.telephoneNumber = telephoneNumber;
-
-        System.out.println("Contact name: " + this.firstName + " " + this.lastName + "\nTelephone number: " + this.telephoneNumber + "\n");
     }
 
-    public String getName()
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-
-//    public String removeContact(){
-//        this.firstName
-//    }
-
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+    
+    public String getFullName(){
+        return (firstName + " " + lastName);
+    }
+    
     public static void main(String[] args){
         Contact contact_1 = new Contact();
         contact_1.setContactInfo("Andrew", "Walsh", "210-456-9090");
