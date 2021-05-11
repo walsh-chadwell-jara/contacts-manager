51c0d181f610cc6f216a162f1072c8a0280337c6
Karla Jara <jara.karla.43@gmail.com>
2021-05-11 10:24:51 -0500

    add new repository

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,19 @@
 package src;
 
 public class Contact {
-    
+    private String firstName;
+    private String lastName;
+    private String telephoneNumber;
+
+    public String getCompleteInfo(){
+        return (firstName + " " + lastName + " " + telephoneNumber);
+    }
+
+    public void setFullName(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Contact name entered " + firstName + " " + lastName);
+    }
+
 }
+
