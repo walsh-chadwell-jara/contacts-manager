017ba3f14d58867338b96a84fe8f7d1082881031
Karla Jara <jara.karla.43@gmail.com>
2021-05-11 12:12:48 -0500

    added to file structure

---
+++ src/contacts/App.java
@@ -0,0 +1,6 @@
+package contacts;
+
+
+public class App{
+
+}
--- src/contacts/Contact.java
+++ src/contacts/Contact.java
@@ -1,4 +1,4 @@
-package src;
+package contacts;
 
 public class Contact {
     private String firstName;
@@ -6,15 +6,17 @@ public class Contact {
     private String telephoneNumber;
 
     public String getCompleteInfo(){
-        return (firstName + " " + lastName + " " + telephoneNumber);
+        return ("Contact name: " + firstName + " " + lastName + "\nTelephone number: " + telephoneNumber + "\n");
     }
 
     public void setContactInfo(String firstName, String lastName, String telephoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
         this.telephoneNumber = telephoneNumber;
-        System.out.println("Contact name entered " + this.firstName + " " + this.lastName + "\n telephone: " + this.telephoneNumber);
+        System.out.println("Contact name: " + this.firstName + " " + this.lastName + "\nTelephone number: " + this.telephoneNumber + "\n");
     }
+
+    public String getName()
 
 //    public String removeContact(){
 //        this.firstName
@@ -25,6 +27,8 @@ public class Contact {
         contact_1.setContactInfo("Andrew", "Walsh", "210-456-9090");
         Contact contact_2 = new Contact();
         contact_2.setContactInfo("Karla", "Jara", "210-369-3333");
+        Contact contact_3 = new Contact();
+        contact_3.setContactInfo("Jake", "Chadwell", "210-4460-4040");
 
 
     }
